9ed1f1256386286d09302e6339f5882bc43bf14f
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-04-09 18:13:28 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,11 @@
+// Last updated: 4/9/2026, 6:13:30 PM
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
