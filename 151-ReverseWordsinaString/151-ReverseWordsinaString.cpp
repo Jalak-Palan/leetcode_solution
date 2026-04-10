6cc5cea8bc47b14b5c8b41feebb5bd6a9289a3a1
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-04-10 15:47:25 +0530

    File updated at 4/10/2026, 3:47:27 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,14 +1,37 @@
-// Last updated: 4/10/2026, 3:07:34 PM
-1class Solution {
-2public:
-3    string reverseWords(string s) {
-4          stringstream ss(s);
-5        string word, result = "";
-6
-7        while (ss >> word) {
-8            result = word + (result.empty() ? "" : " " + result);
-9        }
-10
-11        return result;
-12    }
-13};
+// Last updated: 4/10/2026, 3:47:27 PM
+1#include <bits/stdc++.h>
+2using namespace std;
+3
+4class Solution {
+5public:
+6    string reverseWords(string s) {
+7        vector<string> store;
+8        string word = "";
+9
+10        // Step 1: Extract words
+11        for (int i = 0; i < s.length(); i++) {
+12            if (s[i] != ' ') {
+13                word += s[i];
+14            } else {
+15                if (word != "") {
+16                    store.push_back(word);
+17                    word = "";
+18                }
+19            }
+20        }
+21
+22        // last word
+23        if (word != "") {
+24            store.push_back(word);
+25        }
+26
+27        // Step 2: reverse and join
+28        string result = "";
+29        for (int i = store.size() - 1; i >= 0; i--) {
+30            result += store[i];
+31            if (i != 0) result += " ";
+32        }
+33
+34        return result;
+35    }
+36};
