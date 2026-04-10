552862283147a4443311fa42167a6b101f67dda5
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-04-10 16:01:39 +0530

    File updated at 4/10/2026, 4:01:41 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,37 +1,33 @@
-// Last updated: 4/10/2026, 3:54:52 PM
-1#include <bits/stdc++.h>
-2using namespace std;
-3
-4class Solution {
-5public:
-6    string reverseWords(string s) {
-7        vector<string> store;
-8        string word = "";
-9
-10        // Step 1: Extract words
-11        for (int i = 0; i < s.length(); i++) {
-12            if (s[i] != ' ') {
-13                word += s[i];
-14            } else {
-15                if (word != "") {
-16                    store.push_back(word);
-17                    word = "";
-18                }
-19            }
+// Last updated: 4/10/2026, 4:01:41 PM
+1class Solution {
+2public:
+3    string reverseWords(string s) {
+4        vector<string> words;
+5        string temp = "";
+6
+7        for (int i = 0; i < s.size(); i++) {
+8            if (s[i] == ' ') {
+9                if (!temp.empty()) {
+10                    words.push_back(temp);
+11                    temp = "";
+12                }
+13            } else {
+14                temp += s[i];
+15            }
+16        }
+17// edge case
+18        if (!temp.empty()) {
+19            words.push_back(temp);
 20        }
 21
-22        // last word
-23        if (word != "") {
-24            store.push_back(word);
-25        }
-26
-27        // Step 2: reverse and join
-28        string result = "";
-29        for (int i = store.size() - 1; i >= 0; i--) {
-30            result += store[i];
-31            if (i != 0) result += " ";
-32        }
-33
-34        return result;
-35    }
-36};
+22        reverse(words.begin(), words.end());
+23        
+24        string result = "";
+25        for (int i = 0; i < words.size(); i++) {
+26            result += words[i];
+27            if (i != words.size() - 1) result += " ";
+28        }
+29
+30        return result;
+31    }
+32};
