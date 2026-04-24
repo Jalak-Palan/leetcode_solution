70e080046bf77a1d97b1f30d7fb433620c871c18
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-04-24 09:49:28 +0530

    File updated at 4/24/2026, 9:49:35 AM

--- 125-ValidPalindrome/125-ValidPalindrome.cpp
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -1,28 +1,28 @@
-// Last updated: 4/24/2026, 9:47:31 AM
+// Last updated: 4/24/2026, 9:49:35 AM
 1class Solution {
 2public:
 3    bool isPalindrome(string s) {
-4        string clean = "";
+4        string word = "";
 5
 6        for (char ch : s) {
-7            // Check valid character
+7            // valid character check
 8            if ((ch >= 'a' && ch <= 'z') || 
 9                (ch >= 'A' && ch <= 'Z') || 
 10                (ch >= '0' && ch <= '9')) {
 11
-12                // Convert uppercase to lowercase
+12                // uppercase -> lowercase
 13                if (ch >= 'A' && ch <= 'Z') {
 14                    ch = ch + 32;
 15                }
 16
-17                clean += ch;
+17                word += ch;
 18            }
 19        }
 20
-21        // Palindrome check
-22        int n = clean.size();
+21        // palindrome check
+22        int n = word.size();
 23        for (int i = 0; i < n / 2; i++) {
-24            if (clean[i] != clean[n - i - 1]) {
+24            if (word[i] != word[n - i - 1]) {
 25                return false;
 26            }
 27        }
