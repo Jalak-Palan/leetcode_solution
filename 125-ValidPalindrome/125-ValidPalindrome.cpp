db023adb5732d64c849da7e01a95a67551747927
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-04-24 09:47:23 +0530

    File updated at 4/24/2026, 9:47:31 AM

--- 125-ValidPalindrome/125-ValidPalindrome.cpp
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -1,27 +1,32 @@
-// Last updated: 4/24/2026, 9:45:53 AM
+// Last updated: 4/24/2026, 9:47:31 AM
 1class Solution {
 2public:
 3    bool isPalindrome(string s) {
 4        string clean = "";
 5
-6        // Step 1: filter + lowercase
-7        for (char c : s) {
-8            if ((c >= 'a' && c <= 'z') || 
-9                (c >= 'A' && c <= 'Z') || 
-10                (c >= '0' && c <= '9')) {
-11                
-12                clean += tolower(c);
-13            }
-14        }
-15
-16        // Step 2: check palindrome
-17        int n = clean.length();
-18        for (int i = 0; i < n / 2; i++) {
-19            if (clean[i] != clean[n - i - 1]) {
-20                return false;
-21            }
-22        }
-23
-24        return true;
-25    }
-26};
+6        for (char ch : s) {
+7            // Check valid character
+8            if ((ch >= 'a' && ch <= 'z') || 
+9                (ch >= 'A' && ch <= 'Z') || 
+10                (ch >= '0' && ch <= '9')) {
+11
+12                // Convert uppercase to lowercase
+13                if (ch >= 'A' && ch <= 'Z') {
+14                    ch = ch + 32;
+15                }
+16
+17                clean += ch;
+18            }
+19        }
+20
+21        // Palindrome check
+22        int n = clean.size();
+23        for (int i = 0; i < n / 2; i++) {
+24            if (clean[i] != clean[n - i - 1]) {
+25                return false;
+26            }
+27        }
+28
+29        return true;
+30    }
+31};
