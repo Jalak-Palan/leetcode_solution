0ad36dc6ef519da3d58a8e2a1c9222fc670dc03e
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-09 17:57:50 +0530

    File updated at 5/9/2026, 5:58:04 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,21 +1,20 @@
-// Last updated: 4/9/2026, 6:13:25 PM
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int ans = 0;
-        int count = 0;
-
-        for(int i = 0;i<nums.size();i++){
-            if(count==0){
-                ans=nums[i];
-            }
-            if (nums[i] == ans){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        return ans;
-    }
-};
+// Last updated: 5/9/2026, 5:58:04 PM
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4        int ans = nums[0];
+5        int count = 1;
+6        for(int i = 1; i< nums.size();i++){
+7            if(count == 0){
+8                ans = nums[i];
+9            }
+10            if(ans == nums[i]){
+11                count++;
+12            }
+13            else{
+14                count--;
+15            }
+16        }
+17        return ans;
+18    }
+19};
