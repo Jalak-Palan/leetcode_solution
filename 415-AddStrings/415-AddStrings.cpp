72d1b1d65a4940654637536601ed315fc2bc48ab
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-12 17:41:24 +0530

    File updated at 5/12/2026, 5:41:41 PM

--- 415-AddStrings/415-AddStrings.cpp
+++ 415-AddStrings/415-AddStrings.cpp
@@ -1,43 +1,28 @@
-// Last updated: 5/12/2026, 5:23:42 PM
+// Last updated: 5/12/2026, 5:41:41 PM
 1class Solution {
 2public:
 3    string addStrings(string num1, string num2) {
-4
-5        int i = num1.length() - 1;
-6        int j = num2.length() - 1;
-7
-8        int carry = 0;
-9
-10        string result = "";
+4        int i = num1.size() - 1;
+5        int j = num2.size() - 1;
+6        int carry = 0;
+7        string ans = "";
+8
+9        while(i>=0 || j>=0 || carry){
+10            int sum = carry;
 11
-12        while (i >= 0 || j >= 0) {
-13
-14            int digit1 = 0;
-15            int digit2 = 0;
-16
-17            if (i >= 0) {
-18                digit1 = num1[i] - '0';
-19                i--;
-20            }
+12            if(i >= 0){
+13                sum += num1[i] - '0';
+14                i--;
+15            }
+16            if(j >= 0){
+17                sum += num2[j] - '0';
+18                j--;
+19            }
+20            ans += (sum % 10) + '0';
 21
-22            if (j >= 0) {
-23                digit2 = num2[j] - '0';
-24                j--;
-25            }
-26
-27            int sum = digit1 + digit2 + carry;
-28
-29            carry = sum / 10;
-30
-31            result += (sum % 10) + '0';
-32        }
-33
-34        if (carry > 0) {
-35            result += carry + '0';
-36        }
-37
-38        reverse(result.begin(), result.end());
-39
-40        return result;
-41    }
-42};
+22            carry = sum / 10;
+23        }
+24        reverse(ans.begin(), ans.end());
+25        return ans;
+26    }
+27};
