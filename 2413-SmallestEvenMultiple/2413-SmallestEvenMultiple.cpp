85c7b01c9383c88981ac3e63b2e0d4d99f5bf197
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-13 17:33:05 +0530

    Create 2413-SmallestEvenMultiple

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.cpp
@@ -0,0 +1,10 @@
+// Last updated: 5/13/2026, 5:33:22 PM
+1class Solution {
+2public:
+3    int smallestEvenMultiple(int n) {
+4        if(n % 2 == 0){
+5            return n;
+6        }
+7        return n * 2;
+8    }
+9};
