cc0ff4edb99b4caeec427f8b19a607bf71453cb8
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-15 11:53:29 +0530

    File updated at 5/15/2026, 11:53:48 AM

--- 202-HappyNumber/202-HappyNumber.cpp
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -1,23 +1,15 @@
-// Last updated: 5/15/2026, 11:47:57 AM
+// Last updated: 5/15/2026, 11:53:48 AM
 1class Solution {
 2public:
-3    bool isHappy(int n) {
-4      set<int> seen;
+3    int maxProfit(vector<int>& prices) {
+4        int mini = prices[0];
 5
-6      while(n !=1 && seen.count(n)==0){
-7        seen.insert(n);
-8
-9        int sum = 0;
-10
-11        while(n>0){
-12            int digit = n % 10;
-13            sum += digit * digit;
-14
-15            n = n/10;
-16        }
-17        n = sum;
-18      }
-19
-20      return n==1;
-21    }
-22};
+6        int profit = 0;
+7
+8        for(int i = 1; i<prices.size();i++){
+9            profit = max(profit, prices[i] - mini);
+10            mini = min(mini, prices[i]);
+11        }
+12        return profit;
+13    }
+14};
