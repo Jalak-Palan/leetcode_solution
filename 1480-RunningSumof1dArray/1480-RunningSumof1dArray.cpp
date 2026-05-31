c51ac4cb03b32acc6202f5a7c56ee061e041a4e3
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-31 20:54:28 +0530

    File updated at 5/31/2026, 8:55:03 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
@@ -1,9 +1,9 @@
-// Last updated: 5/20/2026, 7:10:48 PM
+// Last updated: 5/31/2026, 8:55:03 PM
 1class Solution {
 2public:
 3    vector<int> runningSum(vector<int>& nums) {
-4    partial_sum(nums.begin(), nums.end(), nums.begin());
+4        partial_sum(nums.begin(), nums.end(), nums.begin());
 5
-6    return nums;
+6        return nums;
 7    }
 8};
