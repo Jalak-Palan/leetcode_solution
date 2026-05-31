b020a1de8cefbbe3f49d69849b3fd303fdc35970
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-31 20:59:42 +0530

    File updated at 5/31/2026, 9:00:17 PM

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
@@ -1,7 +1,7 @@
-// Last updated: 5/20/2026, 7:08:12 PM
+// Last updated: 5/31/2026, 9:00:17 PM
 1class Solution {
 2public:
 3    int countOdds(int low, int high) {
-4        return (high+1)/2 - low/2;
+4        return (high + 1)/2 - low / 2;
 5    }
 6};
