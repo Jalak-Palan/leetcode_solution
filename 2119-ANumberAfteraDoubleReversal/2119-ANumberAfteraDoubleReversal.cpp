569d65237da83227d7a550d464146192ce801fa6
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-05-31 20:48:10 +0530

    File updated at 5/31/2026, 8:48:44 PM

--- 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.cpp
+++ 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.cpp
@@ -1,7 +1,7 @@
-// Last updated: 5/20/2026, 7:03:44 PM
+// Last updated: 5/31/2026, 8:48:44 PM
 1class Solution {
 2public:
 3    bool isSameAfterReversals(int num) {
-4        return num ==0 || num %10 != 0;
+4        return num == 0 || num % 10 != 0;
 5    }
 6};
