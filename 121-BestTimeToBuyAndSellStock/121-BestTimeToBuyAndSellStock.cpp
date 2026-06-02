bed1f0cba39f2fd22e299835f2d71f08bb0ba576
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-06-02 09:47:14 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/2/2026, 9:47:50 AM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+
+        int profit = 0;
+
+        for(int i = 1; i<prices.size();i++){
+            profit = max(profit, prices[i] - mini);
+            mini = min(mini, prices[i]);
+        }
+        return profit;
+    }
+};
