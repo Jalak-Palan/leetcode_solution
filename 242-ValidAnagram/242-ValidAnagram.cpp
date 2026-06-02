a3ad56b42c08b3484c094889efeaf3e610b13e09
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-06-02 09:47:07 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/2/2026, 9:47:43 AM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.size() != t.size()){
+        return false;
+       } 
+
+       int count [26] = {0};
+
+       for(int i =0; i<s.size();i++){
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+       }
+
+       for(int i = 0; i< 26;i++){
+        if (count[i] != 0){
+            return false;
+        }
+       }
+       return true;
+    }
+};
