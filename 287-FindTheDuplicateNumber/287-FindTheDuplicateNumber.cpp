48594cf28cceab444ecc47091bd0f3d5d97ba9d2
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-06-02 09:47:02 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/2/2026, 9:47:38 AM
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        // find duplicates
+        slow = nums[0];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
