7124f661ee3b33b1be44b3eb59708133b9e7b285
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-06-12 19:23:16 +0530

    File updated at 6/12/2026, 7:23:22 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.cpp
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -1,23 +1,27 @@
-// Last updated: 6/12/2026, 7:18:32 PM
+// Last updated: 6/12/2026, 7:23:22 PM
 1class Solution {
 2public:
-3    bool isIsomorphic(string s, string t) {
+3    bool isPalindrome(ListNode* head) {
 4
-5        unordered_map<char, char> mp1;
-6        unordered_map<char, char> mp2;
-7
-8        for (int i = 0; i < s.size(); i++) {
-9
-10            if (mp1.count(s[i]) && mp1[s[i]] != t[i])
-11                return false;
-12
-13            if (mp2.count(t[i]) && mp2[t[i]] != s[i])
-14                return false;
-15
-16            mp1[s[i]] = t[i];
-17            mp2[t[i]] = s[i];
-18        }
+5        vector<int> v;
+6
+7        while (head != NULL) {
+8            v.push_back(head->val);
+9            head = head->next;
+10        }
+11
+12        int i = 0;
+13        int j = v.size() - 1;
+14
+15        while (i < j) {
+16
+17            if (v[i] != v[j])
+18                return false;
 19
-20        return true;
-21    }
-22};
+20            i++;
+21            j--;
+22        }
+23
+24        return true;
+25    }
+26};
