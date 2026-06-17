0a776f5bda3cc809f2270030039e50e5a53c6f6c
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-06-17 11:38:00 +0530

    Create 1221-SplitaStringinBalancedStrings

---
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/17/2026, 11:38:10 AM
+1class Solution {
+2public:
+3    int balancedStringSplit(string s) {
+4        int balance = 0;
+5        int count = 0;
+6
+7        for (char ch : s) {
+8            if (ch == 'L') {
+9                balance++;
+10            } else {
+11                balance--;
+12            }
+13
+14            if (balance == 0) {
+15                count++;
+16            }
+17        }
+18
+19        return count;
+20    }
+21};
