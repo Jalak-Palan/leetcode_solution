7376ee45ce7baafcb8dbf5c290e23b47ee05874f
Jalak-Palan <jalak.palan.cg@gmail.com>
2026-09-15 17:01:55 +0530

    File updated at 9/15/2026, 5:02:01 PM

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,12 +1,19 @@
-// Last updated: 5/13/2026, 1:49:04 PM
+// Last updated: 9/15/2026, 5:02:01 PM
 1class Solution {
 2public:
-3    string toLowerCase(string s) {
-4        for(int i =0; i<s.size();i++){
-5            if(s[i]>='A' && s[i]<='Z'){
-6                s[i] = s[i] + 32;
-7            }
-8        }
-9            return s;
-10    }
-11};
+3    int reverse(int x) {
+4        long long ans = 0;
+5
+6        while (x != 0) {
+7            int digit = x % 10;
+8            x = x / 10;
+9
+10            ans = ans * 10 + digit;
+11        }
+12
+13        if (ans > INT_MAX || ans < INT_MIN)
+14            return 0;
+15
+16        return ans;
+17    }
+18};
